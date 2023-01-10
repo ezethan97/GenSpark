0fe60fe3737e6d4a274ebff790c3c642f63ee385
ezethan97 <61064947+ezethan97@users.noreply.github.com>
2023-01-10 08:04:06 -0600

    Add files via upload

---
+++ DragonCave/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("You are in a land full of dragons. In front of you,");
+        System.out.println("you see two caves. In one cave, the dragon is friendly");
+        System.out.println("and will share his treasure with you. The other dragon");
+        System.out.println("is greedy and hungry and will eat you on sight.");
+        System.out.println("Which cave will you go into? (1 or 2)");
+        String input = in.nextLine();
+        if (input.equals("1"))
+        {
+            System.out.println("You approach the cave...");
+            System.out.println("It is dark and spooky...");
+            System.out.println("A large dragon jumps out in front of you! He opens his jaws and...");
+            System.out.println("Gobbles you down in one bite!");
+        }
+        if (input.equals("2"))
+        {
+            System.out.println("You approach the cave...");
+            System.out.println("It is dark and spooky...");
+            System.out.println("A large dragon jumps out in front of you! He opens his jaws and...");
+            System.out.println("Tells you where his treasure is!");
+        }
+
+
+    }
+}
